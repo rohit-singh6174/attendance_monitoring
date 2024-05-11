d4abca32eac8ab73f842395bce3b2f2d206ebf35
Rohit Singh <rohit224455@gmail.com>
2024-05-11 15:23:52 +0530

    New Welcome Page

--- teacher/views.py
+++ teacher/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,"teacher/welcome.html")
+    return render(request,"teacher/index.html")
